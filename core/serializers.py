74a6e125db033ddb3c45b3829b4568f9bd40297d
yaya-soumah <yaya-soumah@outlook.com>
2025-04-30 13:49:20 +0800

    mod: user management updated

--- core/serializers.py
+++ core/serializers.py
@@ -1,7 +1,24 @@
 from rest_framework import serializers
+from users.models import User
 
-class LoginSerializer(serializers.Serializer):
+class LoginSerializer(serializers.ModelSerializer):
+    # Input fields
     username = serializers.CharField(required=True)
     password = serializers.CharField(required=True, write_only=True)
+    # Output fields
+    id = serializers.IntegerField(read_only=True)
+    role_name=serializers.CharField(source='profile.role.name', read_only=True, allow_null=True)
+    email=serializers.EmailField(read_only=True)
+    token = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username','password','email','role_name','token']
+    
+    def get_token(self, obj):
+        from rest_framework.authtoken.models import Token
+        token, _ = Token.objects.get_or_create(user=obj)
+        return f"Bearer {token}"
+
 
     
